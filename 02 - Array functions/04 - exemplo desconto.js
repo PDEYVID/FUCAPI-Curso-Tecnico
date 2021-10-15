07af5f9a6e817e154f6f8ec8591d870e0004d361
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2021-10-15 03:47:20 -0300

    Create 04 - exemplo desconto.js

---
+++ 02 - Array functions/04 - exemplo desconto.js
@@ -0,0 +1,38 @@
+var produtos = [{
+        nome: "arroz",
+        preco: 5.50
+    },
+    {
+        nome: "farinha",
+        preco: 10.0
+    },
+    {
+        nome: " feijao",
+        preco: 8.40
+    }
+]
+
+function desconto(produto, perc) {
+    // produto deve estar na forma: {nome: "", preco: 0.0}
+    // perc -> percentual de desconto: entre 0 a 1
+
+    produto ={
+        nome: produto.nome,
+        preco:  produto.preco - produto.preco*perc
+    }
+    return produto;
+}
+
+let p1 = { nome: "feijão", preco: 8.40}
+let p1ComDesconto = desconto(p1, 0.15)
+let p2 = desconto(p1,0.15)
+
+console.log(p1)
+console.log(p1ComDesconto)
+console.log(p2)
+
+listaDesconto = produtos.map( function (produto){
+    return desconto (produto, 0.1)
+})
+console.log(produtos)
+console.log(listaDesconto)
