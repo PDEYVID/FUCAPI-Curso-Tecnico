1051c6845e36b66a153429b8982d9143d0b9917f
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2021-10-15 03:47:11 -0300

    Create 02 - map.js

---
+++ 02 - Array functions/02 - map.js
@@ -0,0 +1,36 @@
+/*
+crie um array numero=[1,2,3] e depois construa um array
+que tenha o dobro de cada elemento.
+*/
+
+/* Exemplo sem Array.map() */
+
+let numeros = [1, 2, 3]
+
+let dobro = [];
+
+for (let i = 0; i < numeros.length; i++) {
+    dobro.push(numeros[i] * 2)
+
+    console.log(numeros[i])
+}
+console.log(dobro)
+
+
+
+
+
+dobro = []
+numeros.push(4)
+// map() -> retorna um array 
+dobro = numeros.map(function (num) {
+    return num * 2
+
+})
+console.log(dobro)
+
+
+
+
+
+console.log(numeros)
